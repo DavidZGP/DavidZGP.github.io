3ff862c2e59d3a31f0a3da3afdeb887482f73a43
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-11-18 18:49:41 -0500

    Guardando Archivos en Carpetas

--- Ejemplos Python 2.py
+++
@@ -1,5 +0,0 @@
-nombre = "David"
-edad = 20
-edad2 = edad + 5
-edad3 = edad2 /2
-print(f"Hola, {nombre}. Tienes {edad3} años.")
--- mi_tortuga.py
+++
@@ -1,6 +0,0 @@
-import turtle
-Pasos= int(input("Ingrese el número de pasos que desea que avance la tortuga: "))
-t = turtle.Turtle()   # Crea una tortuga
-print("la tortuga avanzará", Pasos, "pasos")
-t.forward(Pasos)        # Avanza 100 unidades
-turtle.done()         # Mantiene la ventana abierta
