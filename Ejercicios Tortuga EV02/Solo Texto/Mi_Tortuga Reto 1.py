03a421502e554ac17c14f92452df205455914a5c
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-11-19 15:33:37 -0500

    Reto 1

---
+++ Ejercicios Tortuga EV02/Solo Texto/Mi_Tortuga Reto 1.py
@@ -0,0 +1,6 @@
+print("Simulador de tortuga en texto")  # Título del programa
+
+pasos = int(input("¿Cuántos pasos quieres que avance la tortuga? "))   # Solicitar al usuario el número de pasos
+
+print("Creando una tortuga simulada que da" ,pasos, "pasos...")  # Indicar la acción que se va a realizar
+print("-" * pasos + ">") # Dibujar la tortuga en texto con la cantidad de pasos especificada
