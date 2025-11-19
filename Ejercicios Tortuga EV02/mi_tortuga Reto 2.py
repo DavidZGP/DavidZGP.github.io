7777a6be8cbd690f4cb8b5ce7092bdc11c2d9492
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-11-18 19:22:12 -0500

    Realización Reto 2. Cambio Nombre reto 1

---
+++ Ejercicios Tortuga EV02/mi_tortuga Reto 2.py
@@ -0,0 +1,7 @@
+import turtle
+Pasos= int(input("Ingrese el número de pasos que desea que avance la tortuga: "))
+t = turtle.Turtle()   # Crea una tortuga
+print("la tortuga avanzará", Pasos, "pasos")
+t.right(90)
+t.forward(Pasos)        # Avanza 100 unidades
+turtle.done()         # Mantiene la ventana abierta
