f9b4a9358c22850f48888a5b81ab445b660e32ef
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-11-19 22:25:24 -0500

    Reto 5

---
+++ Ejercicios Tortuga EV02/Con Funciones/Mi_Tortuga Reto 5.py
@@ -0,0 +1,25 @@
+print ("reto5_tortuga")
+pos_x = 0  # posición horizontal acumulada (columna donde termina la flecha '>')
+
+def adelante(pasos): # Función para mover la tortuga hacia adelante
+    global pos_x  # usamos la variable global pos_x
+    # dibuja la línea horizontal desplazada por pos_x
+    print(" " * pos_x + "-" * pasos + ">")
+ 
+    pos_x += pasos  # actualiza la posición horizontal
+
+def abajo(pasos): # Función para mover la tortuga hacia abajo
+  
+    for _ in range(pasos): 
+        print(" " * pos_x + "|")  # dibuja las líneas verticales desplazadas por pos_x
+    print(" " * pos_x + "v")  
+
+
+adelante(4)
+abajo(2)
+
+adelante(5)
+abajo(3)
+
+adelante(3)
+abajo(4)
