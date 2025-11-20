ae0ec27adb4497a2d9275c52231cd05da34251b7
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-11-20 18:08:37 -0500

    Agregando Explicaciones

--- Ejercicios Tortuga EV02/Grafico/mi_tortuga Reto 1.py
+++ Ejercicios Tortuga EV02/Grafico/mi_tortuga Reto 1.py
@@ -1,6 +1,6 @@
 import turtle
-Pasos= int(input("Ingrese el número de pasos que desea que avance la tortuga: "))
+Pasos= int(input("Ingrese el número de pasos que desea que avance la tortuga: ")) # Solicitar al usuario el número de pasos
 t = turtle.Turtle()   # Crea una tortuga
-print("la tortuga avanzará", Pasos, "pasos")
-t.forward(Pasos)        # Avanza 100 unidades
+print("la tortuga avanzará", Pasos, "pasos")  # Indicar la acción que se va a realizar
+t.forward(Pasos)        # Avanza las Unidades indicadas por el usuario
 turtle.done()         # Mantiene la ventana abierta
